d275109274c61834d56baeb3aed5806c62cfa1d9
Kreverson <kreversonjulho@gmail.com>
2025-06-30 13:45:15 -0300

    Adjusted provider for apply helm elb

--- eks/data.tf
+++ eks/data.tf
@@ -31,7 +31,3 @@ data "aws_subnet" "private" {
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
 }
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
